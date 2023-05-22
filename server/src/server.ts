7859eb043a5b8cf1c58997a6c7e9279dfb9d86a8
Victor Pagnozi <victor.pagnozi@gmail.com>
2023-05-21 22:23:19 -0300

    Done backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,40 @@
-import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import 'dotenv/config'
 
-const app = fastify();
-const prisma = new PrismaClient();
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
-app.get("/hello", async () => {
-  const users = await prisma.user.findMany();
+const app = fastify()
 
-  return users;
-});
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
+app.register(cors, {
+  origin: true,
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
-    console.log("🎉 HTTP server running on http://localhost:3333");
-  });
+    console.log('🚀 HTTP server running on port http://localhost:3333')
+  })
